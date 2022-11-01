fde6eec4cb21a4da0678a8903f6f6c6204c2c1e5
SrGnis <gnisms97@gmail.com>
2022-11-01 18:05:53 +0100

    Screen test

---
+++ src/main/java/net/fabricmc/fabric/test/screenhandler/item/EntitiedBagItem.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.screenhandler.item;
+
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.fabricmc.fabric.test.screenhandler.screen.EntitiedBagScreenHandler;
+import net.fabricmc.fabric.test.screenhandler.screen.PositionedBagScreenHandler;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class EntitiedBagItem extends BagItem {
+	public EntitiedBagItem(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		ItemStack stack = user.getStackInHand(hand);
+		user.openHandledScreen(createScreenHandlerFactory(stack, user));
+		return TypedActionResult.success(stack);
+	}
+
+	@Override
+	public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+		user.openHandledScreen(createScreenHandlerFactory(stack, entity));
+		return ActionResult.SUCCESS;
+	}
+
+	private ExtendedScreenHandlerFactory createScreenHandlerFactory(ItemStack stack, LivingEntity entity) {
+		return new ExtendedScreenHandlerFactory() {
+			@Override
+			public ScreenHandler createMenu(int syncId, PlayerInventory inventory, PlayerEntity player) {
+				return new EntitiedBagScreenHandler(syncId, inventory, new BagInventory(stack), entity);
+			}
+
+			@Override
+			public Text getDisplayName() {
+				return stack.getName();
+			}
+
+			@Override
+			public void writeScreenOpeningData(ServerPlayerEntity player, PacketByteBuf buf) {
+				buf.writeInt(entity.getId());
+			}
+		};
+	}
+}
---
+++ src/main/java/net/fabricmc/fabric/test/screenhandler/screen/EntitiedBagScreenHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.screenhandler.screen;
+
+import net.fabricmc.fabric.test.screenhandler.ScreenHandlerTest;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.UUID;
+
+public class EntitiedBagScreenHandler extends BagScreenHandler {
+	private final LivingEntity entity;
+
+	public EntitiedBagScreenHandler(int syncId, PlayerInventory playerInventory, PacketByteBuf buf) {
+		this(syncId, playerInventory, new SimpleInventory(9), readEntity(buf, playerInventory.player.world));
+	}
+
+	public EntitiedBagScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, LivingEntity entity) {
+		super(ScreenHandlerTest.POSITIONED_BAG_SCREEN_HANDLER, syncId, playerInventory, inventory);
+		this.entity = entity;
+	}
+
+	private static LivingEntity readEntity(PacketByteBuf buf, World world) {
+		int id = buf.readInt();
+		return (LivingEntity) world.getEntityById(id);
+	}
+
+	public LivingEntity getEntity() {
+		return entity;
+	}
+}
